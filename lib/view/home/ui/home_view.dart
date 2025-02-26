9ba0857fbaaeff393714346ea6eabf1a8788c1e7
salah <salahswidan212@gmail.com>
2025-02-26 16:33:14 +0200

    add cateories list to home view

--- lib/view/home/ui/home_view.dart
+++ lib/view/home/ui/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import '../../../core/app_colors.dart';
 import '../../../core/components/custom_search_field.dart';
@@ -13,10 +14,81 @@ class HomeView extends StatelessWidget {
         children: [
           CustomSearchField(),
           const SizedBox(
-            height: 20,),
-            Image.asset("assets/images/buy.jpg")
+            height: 20,
+          ),
+          Image.asset("assets/images/buy.jpg"),
+          SizedBox(
+            height: 15,
+          ),
+          Text(
+            "Popular Categories",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          SizedBox(
+            height: 100,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: AppColors.kPrimaryColor,
+                        foregroundColor: AppColors.kWhiteColor,
+                        child: Icon(
+                          categories[index].icon,
+                          size: 40,
+                        ),
+                      ),
+                      Text(categories[index].text),
+                    ],
+                  ),
+                );
+              },
+              itemCount: categories.length,
+            ),
+          )
         ],
       ),
     );
   }
+}
+
+List<Category> categories = [
+  Category(
+    text: "Sports",
+    icon: Icons.sports,
+  ),
+  Category(
+    text: "Electronics",
+    icon: Icons.tv,
+  ),
+  Category(text: "Collections", icon: Icons.collections),
+  Category(
+    text: "Books",
+    icon: Icons.book,
+  ),
+  Category(
+    text: "Games",
+    icon: Icons.games,
+  ),
+  Category(
+    text: "Bikes",
+    icon: Icons.bike_scooter,
+  ),
+];
+
+class Category {
+  final String text;
+  final IconData icon;
+
+  Category({required this.text, required this.icon});
 }
