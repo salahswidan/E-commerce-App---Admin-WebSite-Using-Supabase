ca8497c54eebf4fb0a0909a93b9372e465afbc68
salah <salahswidan212@gmail.com>
2025-02-27 13:41:03 +0200

    finish product cart ui

--- lib/view/home/ui/home_view.dart
+++ lib/view/home/ui/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:our_market/view/auth/ui/widget/custom_elevated_btn.dart';
 import '../../../core/app_colors.dart';
 import '../../../core/components/custom_search_field.dart';
 import 'widget/categories_list.dart';
@@ -43,7 +44,7 @@ class HomeView extends StatelessWidget {
               children: [
                 Stack(
                   children: [
-                    ClipRRect(
+                    const ClipRRect(
                       borderRadius: BorderRadius.only(
                           topRight: Radius.circular(16),
                           bottomRight: Radius.circular(16),
@@ -61,20 +62,71 @@ class HomeView extends StatelessWidget {
                       alignment: Alignment.center,
                       width: 65,
                       height: 35,
-                      decoration: BoxDecoration(
+                      decoration: const BoxDecoration(
                         color: AppColors.kPrimaryColor,
                         borderRadius: BorderRadius.only(
                           topRight: Radius.circular(16),
                           bottomRight: Radius.circular(16),
                         ),
                       ),
-                      child: Text("10% OFF",
+                      child: const Text("10% OFF",
                           style: TextStyle(
                             color: Colors.white,
                             fontWeight: FontWeight.bold,
                           )),
                     ))
                   ],
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text("Product Name",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              )),
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.favorite,
+                              color: AppColors.kGreyColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Column(
+                            children: [
+                              const Text("100 LE",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  )),
+                              const Text("120 LE",
+                                  style: TextStyle(
+                                      decoration: TextDecoration.lineThrough,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                      color: AppColors.kGreyColor)),
+                            ],
+                          ),
+                          CustomEBtn(
+                            text: "Buy Now",
+                            onTap: () {},
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
