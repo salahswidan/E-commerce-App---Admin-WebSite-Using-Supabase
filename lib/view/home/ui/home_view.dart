565ebf5ade97577a3bb92c6a32d7c61301ee1a91
salah <salahswidan212@gmail.com>
2025-02-27 18:55:18 +0200

    finish store view ui

--- lib/view/home/ui/home_view.dart
+++ lib/view/home/ui/home_view.dart
@@ -1,9 +1,4 @@
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:our_market/view/auth/ui/widget/custom_elevated_btn.dart';
-import '../../../core/app_colors.dart';
-import '../../../core/components/cache_image.dart';
 import '../../../core/components/custom_search_field.dart';
 import '../../../core/components/product_list.dart';
 import 'widget/categories_list.dart';
@@ -39,7 +34,7 @@ class HomeView extends StatelessWidget {
           const SizedBox(
             height: 15,
           ),
-          Text("Recommended Products",
+          const Text("Recommended Products",
               style: TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 20,
@@ -47,7 +42,7 @@ class HomeView extends StatelessWidget {
           const SizedBox(
             height: 15,
           ),
-          ProductList()
+          const ProductList()
         ],
       ),
     );
--- lib/view/store/ui/store_view.dart
+++ lib/view/store/ui/store_view.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
+import '../../../core/components/custom_search_field.dart';
+import '../../../core/components/product_list.dart';
+import '../../home/ui/widget/categories_list.dart';
 
 class StoreView extends StatelessWidget {
   const StoreView({super.key});
 
   @override
   Widget build(BuildContext context) {
-   return Center(
-      child: Text("Store View"),
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ListView(
+        children: const [
+          Center(
+            child: Text(
+              "Welcome To Our Market",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          CustomSearchField(),
+          SizedBox(
+            height: 15,
+          ),
+          ProductList()
+        ],
+      ),
     );
   }
 }
