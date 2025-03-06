4a429161bc7cb7864b5c720a048bf339b46995a8
salah <salahswidan212@gmail.com>
2025-03-06 11:15:05 +0200

    add rating bar for product details view

--- lib/view/product_details/ui/product_details_view.dart
+++ lib/view/product_details/ui/product_details_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:our_market/core/functions/build_appBar.dart';
 
 import '../../../core/components/cache_image.dart';
@@ -15,7 +16,64 @@ class ProductDetailsView extends StatelessWidget {
           CacheImage(
             url:
                 "https://img.freepik.com/premium-vector/ramadan-kareem-islamic-post-social-media-background-template_303121-75.jpg?ga=GA1.1.1482810904.1740580934&semt=ais_hybrid",
-          )
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 10),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Product Name"),
+                    Text("123 LE"),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Text("3 "),
+                        Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                      ],
+                    ),
+                    Icon(
+                      Icons.favorite,
+                      color: Colors.grey,
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Text("Product Description"),
+                SizedBox(
+                  height: 20,
+                ),
+                RatingBar.builder(
+                  initialRating: 3,
+                  minRating: 1,
+                  direction: Axis.horizontal,
+                  allowHalfRating: false,
+                  itemCount: 5,
+                  itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                  itemBuilder: (context, _) => Icon(
+                    Icons.star,
+                    color: Colors.amber,
+                  ),
+                  onRatingUpdate: (rating) {
+                    print(rating);
+                  },
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
