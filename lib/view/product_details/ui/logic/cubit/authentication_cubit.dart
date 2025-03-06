49786923a4d6ce27832d9f9c96faa6df55f60722
salah <salahswidan212@gmail.com>
2025-03-06 20:39:28 +0200

    start auth & add login function

---
+++ lib/view/product_details/ui/logic/cubit/authentication_cubit.dart
@@ -0,0 +1,29 @@
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+part 'authentication_state.dart';
+
+class AuthenticationCubit extends Cubit<AuthenticationState> {
+  AuthenticationCubit() : super(AuthenticationInitial());
+
+  SupabaseClient client = Supabase.instance.client;
+
+  Future<void> login({required String email, required String password}) async {
+    emit(LoginLoading());
+    try {
+      await client.auth.signInWithPassword(email: email, password: password);
+      emit(LoginSuccess());
+    } on AuthException catch (e) {
+      log(e.toString());
+            emit(LoginError(e.message));
+    
+    } catch (e) {
+            log(e.toString());
+
+      emit(LoginError(e.toString()));
+    }
+  }
+}
---
+++ lib/view/product_details/ui/logic/cubit/authentication_state.dart
@@ -0,0 +1,20 @@
+part of 'authentication_cubit.dart';
+
+@immutable
+sealed class AuthenticationState {}
+
+final class AuthenticationInitial extends AuthenticationState {}
+
+final class LoginSuccess extends AuthenticationState {}
+final class LoginLoading extends AuthenticationState {}
+final class LoginError extends AuthenticationState {
+  final String message;
+  LoginError( this.message);
+}
+
+final class SignUpSuccess extends AuthenticationState {}
+final class SignUpLoading extends AuthenticationState {}
+final class SignUpError extends AuthenticationState {
+  final String message;
+  SignUpError( this.message);
+}
